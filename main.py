4b2cb27a1b658588a4a349e7eff6cde27a3c4295
MrStonkus <mr.valdas.stonkus@gmail.com>
2021-11-07 11:26:20 +0200

    refactored and added new book isbn checking

--- main.py
+++ main.py
@@ -1,41 +1,72 @@
 
+# state could be: pub, library and borrowed.  Depending of the location
+# ISBN contains simple digits without dashes
+class Book:
+    def __init__(self, isbn_nr, title_tx, author_tx, year_int, state='pub'):
+        self.isbn = isbn_nr
+        self.title = title_tx
+        self.author = author_tx
+        self.year = year_int
+        self.state = state
 
 
-# create Distributor class
+# Distributor has new publicised books available for library
 class Distributor:
     def __init__(self):
         self.books = []
 
-    def create_book(self):
-        isbn = input("Enter book's ISBN number: ")
-        title = input("Enter book's title: ")
-        author = input("Enter book's author: ")
-        year = input("Enter year of publication: ")
-        new_book = Book(isbn, title, author, year)
-        self.books.append(new_book)
+    def create_book(self, isbn_nr, title_tx, author_tx, year_int):
+        new_book = Book(isbn_nr, title_tx, author_tx, year_int)
+        if self.check_for_dublicate(new_book.isbn):
+            print("The same ISBN number is in the list!")
+        else:
+            self.books.append(new_book)
 
     def list_books(self):
+        print('------ LIST OF AVAILABLE BOOKS ------')
+        nr = 1
         for book in self.books:
-            print(book)
+            print(f'{nr}. {book.isbn} {book.title} {book.author} {book.year}')
+            nr += 1
 
+    def check_for_dublicate(self, isbn_nr):
+        # get all isbn numbers in list
+        isbn_list = []
+        for book in self.books:
+            isbn_list.append(book.isbn)
+        # check list for the same isbn
+        if isbn_nr in isbn_list:
+            return True
+        else:
+            return False
 
 # Create library class
 
 
-# state could be: root, library, borrowed
-class Book:
-    def __init__(self, isbn, title, author, year, state='root'):
-        self.isbn = isbn
-        self.title = title
-        self.author = author
-        self.year = year
-        self.state = state
+
 
 
 # create menu
 if __name__ == '__main__':
     distributor = Distributor()
 
+    # load default books
+    distributor.create_book(100, 'title1', 'author1', 2001)
+    distributor.create_book(101, 'title2', 'author2', 2002)
+    distributor.create_book(101, 'title3', 'author3', 2003)
+    distributor.create_book(103, 'title4', 'author4', 2004)
+
+
+
+    # console functions
+
+    def get_new_book_inputs():
+
+        isbn_nr = input("Enter book's ISBN number (only numbers): ")
+        title_tx = input("Enter book's title: ")
+        author_tx = input("Enter book's author: ")
+        year_nr = input("Enter year of publication: ")
+        return isbn_nr, title_tx, author_tx, year_nr
 
     isQuit = False
     while not isQuit:
@@ -58,7 +89,8 @@ if __name__ == '__main__':
         else:
             match menu_nr:
                 case 1:
-                    distributor.create_book()
+                    isbn, title, author, year = get_new_book_inputs()
+                    distributor.create_book(isbn, title, author, year)
                 case 2:
                     distributor.list_books()
                 case 3:
